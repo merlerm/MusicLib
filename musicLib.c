ed266e071dc09873b244ed35c1ec2bca68f7a882
Matteo Merler <matteo.merler@gmail.com>
2021-11-10 11:53:59 +0100

    musicLib release v1.0
    Initial commit and release of the musicLib.

---
+++ musicLib.c
@@ -0,0 +1,174 @@
+/******************************************************************************
+ *   This file is part of musicLib.
+ *
+ *   musicLib is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   musicLib is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with musicLib.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+
+#include <musicLib.h>
+#include <ti/devices/msp432p4xx/inc/msp.h>
+#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+#include <stdio.h>
+
+/* GLOBAL values */
+const Note_t *song_array;
+const Note_config_t *config_array;
+
+uint_fast16_t counter = 0;        /* Tracks the current note being played */
+uint_fast16_t songTempo = 0;      /* Global tempo of the song */
+float songVolume = 0;             /* Global volume of the song */
+uint_fast16_t config_index = 1;   /* Index of the current config value */
+uint_fast16_t repetition_counter; /* Tracks the number of repetitions left */
+
+/* Timer_A UpMode Configuration Parameter (used for the buzzer) */
+Timer_A_UpModeConfig timerUpConfig = {
+                                      TIMER_A_CLOCKSOURCE_SMCLK,              // SMCLK Clock Source
+                                      TIMER_A_CLOCKSOURCE_DIVIDER_64,         // SMCLK/1 = 3MHz
+                                      0,                                      // Frequency value
+                                      TIMER_A_TAIE_INTERRUPT_DISABLE,         // Disable Timer interrupt
+                                      TIMER_A_CCIE_CCR0_INTERRUPT_ENABLE,     // Enable CCR0 interrupt
+                                      TIMER_A_DO_CLEAR                        // Clear value
+                                      };
+
+/* Timer_A Compare Configuration Parameter (PWM) */
+Timer_A_CompareModeConfig compareConfig_PWM = {
+                                               TIMER_A_CAPTURECOMPARE_REGISTER_4,          // Use CCR3
+                                               TIMER_A_CAPTURECOMPARE_INTERRUPT_DISABLE,   // Disable CCR interrupt
+                                               TIMER_A_OUTPUTMODE_TOGGLE_SET,              // Toggle output but
+                                               0                                           // Volume Duty Cycle
+                                               };
+
+/* Timer_A Up Configuration Parameter (used for tempo) */
+Timer_A_UpModeConfig upConfig = {
+                                 TIMER_A_CLOCKSOURCE_SMCLK,              // SMCLK = 3 MhZ
+                                 TIMER_A_CLOCKSOURCE_DIVIDER_1,          // SMCLK/12 = 250 KhZ
+                                 0,                                      // Tick period
+                                 TIMER_A_TAIE_INTERRUPT_DISABLE,         // Disable Timer interrupt
+                                 TIMER_A_CCIE_CCR0_INTERRUPT_DISABLE,    // Disable CCR0 interrupt
+                                 TIMER_A_DO_CLEAR                        // Clear value
+                                 };
+
+void _buzzerInit()
+{
+    /* Configures P2.7 to PM_TA0.4 for using Timer PWM to control the buzzer */
+    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P2, GPIO_PIN7, GPIO_PRIMARY_MODULE_FUNCTION);
+    /* Configuring Timer_A0 for Up Mode */
+    Timer_A_configureUpMode(TIMER_A0_BASE, &upConfig);
+}
+
+void _timerInit()
+{
+    /* Configuring Timer_A1 for Up Mode */
+    Timer_A_configureUpMode(TIMER_A1_BASE, &timerUpConfig);
+    /* Enabling interrupts */
+    Interrupt_enableInterrupt(INT_TA1_0);
+}
+
+/**
+ * Initializes the specific hardware (like peripherals, clocks, timers) of the embedded system for playing songs with the buzzer.
+ */
+void _musicLibInit()
+{
+    _buzzerInit();
+    _timerInit();
+}
+
+#define VOLUME_LOWERBOUND 15   /* Volume threshold under which the volume is too low to hear */
+
+uint_fast16_t max(uint_fast16_t num1, uint_fast16_t num2) {
+    return (num1 > num2 ) ? num1 : num2;
+}
+
+/* Counter used to skip clock cycles and make the song slower, proportional to the
+ * longest note value. Used to have slower songs and prevent overflow on the timer value */
+uint_fast16_t skip = 0;
+
+/* Timer handler where the single notes are played */
+void TA1_0_IRQHandler(void)
+{
+    if (skip == 0) /* Check for skip cycle */
+    {
+        if (config_array[config_index].index == END_CONFIG) /* Reset config if at the end */
+            config_index = 0;
+
+        if (config_array[config_index].index == (counter + 1)) { /* If time to change config values, update */
+            songTempo = config_array[config_index].tempo % TEMPO_UPPERBOUND;
+            songVolume = config_array[config_index].volume;
+            config_index++;
+        }
+
+        /* Update new values for the note that will soon be played */
+        timerUpConfig.timerPeriod = (uint_fast16_t) (song_array[counter].tempo * songTempo); /* Note tempo */
+        Timer_A_configureUpMode(TIMER_A1_BASE, &timerUpConfig);
+        Timer_A_startCounter(TIMER_A1_BASE, TIMER_A_UP_MODE);
+
+        /* Note frequency values */
+        upConfig.timerPeriod = song_array[counter].frequency.clock;
+        upConfig.clockSourceDivider = song_array[counter].frequency.divider;
+
+        /* Note volume (PWM normalized with respect to the note's clock value) */
+        uint_fast16_t note_volume = (uint_fast16_t) ((float) ((song_array[counter].frequency.clock / 100) * songVolume));
+        compareConfig_PWM.compareValue = max(note_volume, VOLUME_LOWERBOUND);
+
+        /* Update timer values */
+        Timer_A_configureUpMode(TIMER_A0_BASE, &upConfig);
+        Timer_A_startCounter(TIMER_A0_BASE, TIMER_A_UP_MODE);
+        Timer_A_initCompare(TIMER_A0_BASE, &compareConfig_PWM);
+
+        /* Advance skip and counter values */
+        skip++;
+        counter++;
+
+        /* End of song check */
+        if (song_array[counter].frequency.clock == 1) {
+            repetition_counter--;
+            if (repetition_counter < 1) { /* Stop song */
+                Timer_A_stopTimer(TIMER_A1_BASE);
+                Timer_A_stopTimer(TIMER_A0_BASE);
+                Timer_A_disableCaptureCompareInterrupt(TIMER_A1_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_0);
+            }
+            /* New repetition */
+            config_index = 0;
+            counter = 0;
+        }
+    }
+    else /* Skip this cycle */
+    {
+        skip = (skip + 1) % DOUBLE;
+    }
+
+    Timer_A_clearCaptureCompareInterrupt(TIMER_A1_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_0);
+}
+
+/**
+ * The main function of the library.
+ * Given a song, a precise config of that song and how many times (repetition) the user wants to hear that song, it plays the melody.
+ */
+void playSong(const Note_t *song, const Note_config_t *config, const uint_fast16_t repetition)
+{
+    /* Initialize song configurations */
+    songTempo = config[0].tempo % TEMPO_UPPERBOUND;
+    timerUpConfig.timerPeriod = songTempo;
+    songVolume = config[0].volume;
+    Timer_A_configureUpMode(TIMER_A1_BASE, &timerUpConfig);
+
+    /* Initialize global variables */
+    song_array = song;
+    config_array = config;
+    repetition_counter = repetition;
+
+    /* Start TIMER_A0_BASE & TIMER_A1_BASE */
+    Timer_A_startCounter(TIMER_A0_BASE, TIMER_A_UP_MODE);
+    Timer_A_initCompare(TIMER_A0_BASE, &compareConfig_PWM);
+    Timer_A_startCounter(TIMER_A1_BASE, TIMER_A_UP_MODE);
+}
---
+++ musicLib.h
@@ -0,0 +1,418 @@
+/******************************************************************************
+ *   This file is part of musicLib.
+ *
+ *   musicLib is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   musicLib is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with musicLib.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+
+#ifndef MUSICLIB_H_ /* MUSICLIB_H_ */
+#define MUSICLIB_H_ /* MUSICLIB_H_ */
+
+#include <ti/devices/msp432p4xx/inc/msp.h>
+#include <stdint.h>
+
+/******************************************************************************
+*                          For more in depth details,                         *
+*                           check out the repo wiki                           *
+*              at https://github.com/Merlo17/MusicLib/wiki/1.-Home            *
+******************************************************************************/
+
+/******************************************************************************
+*                                    PAUSE                                    *
+******************************************************************************/
+#define PAUSE 0,     TIMER_A_CLOCKSOURCE_DIVIDER_1
+
+/******************************************************************************
+*                              SONG END MARKER                                *
+******************************************************************************/
+#define END   1,     TIMER_A_CLOCKSOURCE_DIVIDER_1
+
+/******************************************************************************
+*                              CONFIG END MARKER                              *
+******************************************************************************/
+#define END_CONFIG       0
+
+/******************************************************************************
+*                                RANGE 2 NOTES                                *
+******************************************************************************/
+#define DO2   22934, TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define C2    DO2
+#define SIs1  DO2
+#define Bs1   DO2
+/******************************************************************************/
+#define DOs2  21646, TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define Cs2   DOs2
+#define REf2  DOs2
+#define Df2   DOs2
+/******************************************************************************/
+#define RE2   20431, TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define D2    RE2
+/******************************************************************************/
+#define REs2  19285,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define Ds2   REs2
+#define MIf2  REs2
+#define Ef2   REs2
+/******************************************************************************/
+#define MI2   18202,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define E2    MI2
+#define FAf2  MI2
+#define Ff2   MI2
+/******************************************************************************/
+#define FA2   17181,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define F2    FA2
+#define MIs2  FA2
+#define Es2   FA2
+/******************************************************************************/
+#define FAs2  16216,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define Fs2   FAs2
+#define SOLf2 FAs2
+#define Gf2   FAs2
+/******************************************************************************/
+#define SOL2  15306,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define G2    SOL2
+/******************************************************************************/
+#define SOLs2 14447,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define Gs2   SOLs2
+#define LAf2  SOLs2
+#define Af2   SOLs2
+/******************************************************************************/
+#define LA2   13636,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define A2    LA5
+/******************************************************************************/
+#define LAs2  12871,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define As2   LAs2
+#define SIf2  LAs2
+#define Bf2   LAs2
+/******************************************************************************/
+#define SI2   12148,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define B2    SI2
+#define DOf3  SI2
+#define Cf3   SI2
+
+/******************************************************************************
+*                                RANGE 3 NOTES                                *
+******************************************************************************/
+#define DO3   11466, TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define C3    DO3
+#define SIs2  DO3
+#define Bs2   DO3
+/******************************************************************************/
+#define DOs3  10823, TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define Cs3   DOs3
+#define REf3  DOs3
+#define Df3   DOs3
+/******************************************************************************/
+#define RE3   10215, TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define D3    RE3
+/******************************************************************************/
+#define REs3  9642,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define Ds3   REs3
+#define MIf3  REs3
+#define Ef3   REs3
+/******************************************************************************/
+#define MI3   9101,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define E3    MI3
+#define FAf3  MI3
+#define Ff3   MI3
+/******************************************************************************/
+#define FA3   8590,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define F3    FA3
+#define MIs3  FA3
+#define Es3   FA3
+/******************************************************************************/
+#define FAs3  8108,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define Fs3   FAs3
+#define SOLf3 FAs3
+#define Gf3   FAs3
+/******************************************************************************/
+#define SOL3  7653,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define G3    SOL3
+/******************************************************************************/
+#define SOLs3 7223,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define Gs3   SOLs3
+#define LAf3  SOLs3
+#define Af3   SOLs3
+/******************************************************************************/
+#define LA3   6818,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define A3    LA5
+/******************************************************************************/
+#define LAs3  6435,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define As3   LAs3
+#define SIf3  LAs3
+#define Bf3   LAs3
+/******************************************************************************/
+#define SI3   6074,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define B3    SI3
+#define DOf4  SI3
+#define Cf4   SI3
+
+/******************************************************************************
+*                                RANGE 4 NOTES                                *
+******************************************************************************/
+#define DO4   5733,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define C4    DO4
+#define SIs3  DO4
+#define Bs3   DO4
+/******************************************************************************/
+#define DOs4  5412,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define Cs4   DOs4
+#define REf4  DOs4
+#define Df4   DOs4
+/******************************************************************************/
+#define RE4   5107,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define D4    RE4
+/******************************************************************************/
+#define REs4  4821,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define Ds4   REs4
+#define MIf4  REs4
+#define Ef4   REs4
+/******************************************************************************/
+#define MI4   4550,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define E4    MI4
+#define FAf4  MI4
+#define Ff4   MI4
+/******************************************************************************/
+#define FA4   4295,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define F4    FA4
+#define MIs4  FA4
+#define Es4   FA4
+/******************************************************************************/
+#define FAs4  4054,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define Fs4   FAs4
+#define SOLf4 FAs4
+#define Gf4   FAs4
+/******************************************************************************/
+#define SOL4  3826,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define G4    SOL4
+/******************************************************************************/
+#define SOLs4 3611,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define Gs4   SOLs4
+#define LAf4  SOLs4
+#define Af4   SOLs4
+/******************************************************************************/
+#define LA4   3409,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define A4    LA5
+/******************************************************************************/
+#define LAs4  3217,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define As4   LAs4
+#define SIf4  LAs4
+#define Bf4   LAs4
+/******************************************************************************/
+#define SI4   3037,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define B4    SI4
+#define DOf5  SI4
+#define Cf5   SI4
+
+/******************************************************************************
+*                                RANGE 5 NOTES                                *
+******************************************************************************/
+#define DO5   2866,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define C5    DO5
+#define SIs4  DO5
+#define Bs4   DO5
+/******************************************************************************/
+#define DOs5  2705,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define Cs5   DOs5
+#define REf5  DOs5
+#define Df5   DOs5
+/******************************************************************************/
+#define RE5   2553,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define D5    RE5
+/******************************************************************************/
+#define REs5  2410,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define Ds5   REs5
+#define MIf5  REs5
+#define Ef5   REs5
+/******************************************************************************/
+#define MI5   2275,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define E5    MI5
+#define FAf5  MI5
+#define Ff5   MI5
+/******************************************************************************/
+#define FA5   2147,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define F5    FA5
+#define MIs5  FA5
+#define Es5   FA5
+/******************************************************************************/
+#define FAs5  2027,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define Fs5   FAs5
+#define SOLf5 FAs5
+#define Gf5   FAs5
+/******************************************************************************/
+#define SOL5  1913,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define G5    SOL5
+/******************************************************************************/
+#define SOLs5 1825,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define Gs5   SOLs5
+#define LAf5  SOLs5
+#define Af5   SOLs5
+/******************************************************************************/
+#define LA5   1704,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define A5    LA5
+/******************************************************************************/
+#define LAs5  1608,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define As5   LAs5
+#define SIf5  LAs5
+#define Bf5   LAs5
+/******************************************************************************/
+#define SI5   1518,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define B5    SI5
+#define DOf6  SI5
+#define Cf6   SI5
+
+/******************************************************************************
+*                                RANGE 6 NOTES                                *
+******************************************************************************/
+#define DO6   1433, TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define C6    DO6
+#define SIs5  DO6
+#define Bs5   DO6
+/******************************************************************************/
+#define DOs6  1352, TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define Cs6   DOs6
+#define REf6  DOs6
+#define Df6   DOs6
+/******************************************************************************/
+#define RE6   1276, TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define D6    RE6
+/******************************************************************************/
+#define REs6  1205,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define Ds6   REs6
+#define MIf6  REs6
+#define Ef6   REs6
+/******************************************************************************/
+#define MI6   1137,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define E6    MI6
+#define FAf6  MI6
+#define Ff6   MI6
+/******************************************************************************/
+#define FA6   1073,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define F6    FA6
+#define MIs6  FA6
+#define Es6   FA6
+/******************************************************************************/
+#define FAs6  1013,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define Fs6   FAs6
+#define SOLf6 FAs6
+#define Gf6   FAs6
+/******************************************************************************/
+#define SOL6  956,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define G6    SOL6
+/******************************************************************************/
+#define SOLs6 902,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define Gs6   SOLs6
+#define LAf6  SOLs6
+#define Af6   SOLs6
+/******************************************************************************/
+#define LA6   852,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define A6    LA5
+/******************************************************************************/
+#define LAs6  804,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define As6   LAs6
+#define SIf6  LAs6
+#define Bf6   LAs6
+/******************************************************************************/
+#define SI6   759,  TIMER_A_CLOCKSOURCE_DIVIDER_1
+#define B6    SI6
+#define DOf7  SI6
+#define Cf7   SI6
+
+/**
+ * Frequency struct - it describes the properties of a single note
+ *
+ * @clock: variable that sets the value the buzzer will play
+ * @divider: variable that sets the clock divider
+ */
+typedef struct Frequency {
+    uint_fast16_t clock;
+    uint_fast16_t divider;
+} Frequency_t;
+
+/******************************************************************************
+*                               TEMPO MULTIPLIERS                             *
+******************************************************************************/
+#define QUARTER          1
+#define HALF             2
+#define WHOLE            4
+#define DOUBLE           8
+#define EIGHTH           0.5
+#define SIXTEENTH        0.25
+#define THIRTYSECOND     0.125
+#define QUARTER_DOT      1.5
+#define HALF_DOT         3
+#define WHOLE_DOT        6
+#define EIGHTH_DOT       0.75
+#define SIXTEENTH_DOT    0.375
+#define THIRTYSECOND_DOT 0.1875
+#define QUARTER_DOT_DOT  1.75
+#define HALF_DOT_DOT     3.5
+#define EIGHTH_DOT_DOT   0.875
+
+/**
+ * Note struct - the single element that composes a song
+ *
+ * @frequency: variable that defines the note itself (so its frequency)
+ * @tempo: variable that defines the duration of the note
+ */
+typedef struct Note {
+    Frequency_t frequency;
+    float tempo;
+} Note_t;
+
+/******************************************************************************
+*                               TEMPO SIGNATURES                              *
+******************************************************************************/
+#define TEMPO_UPPERBOUND 8191
+#define ULTRA_SLOW       8000
+#define VERY_SLOW        7000
+#define SLOW             6000
+#define MEDIUM           5000
+#define FAST             4000
+#define VERY_FAST        3000
+#define ULTRA_FAST       2000
+#define SUPERSONIC       1000
+
+/******************************************************************************
+*                               VOLUME SETTINGS                               *
+******************************************************************************/
+#define PIANISSIMO       1.0f
+#define PIANO            2.0f
+#define MODERATO         4.0f
+#define FORTE            17.5f
+#define FORTISSIMO       30.0f
+
+/**
+ * Note_config struct - it defines the rhythm of groups of notes
+ *
+ * @index: variable that indicates the beginning of the group
+ * @tempo: variable that defines the notes' duration of the group we are considering
+ * @volume: variable that defines the volume of the group we are considering
+ */
+typedef struct Note_config {
+    uint_fast16_t index;
+    uint_fast16_t tempo;
+    float volume;
+} Note_config_t;
+
+/**
+ * Initializes the specific hardware (like peripherals, clocks, timers) of the embedded system for playing songs with the buzzer.
+ */
+void _musicLibInit();
+
+/**
+ * The main function of the library.
+ * Given a song, a precise config of that song and how many times (repetition) the user wants to hear that song, it plays the melody.
+ */
+void playSong(const Note_t *song, const Note_config_t *config, const uint_fast16_t repetition);
+
+#endif /* MUSICLIB_H_ */
